c600e5ee8f91f5ed633e2d02235743795a9a5917
Pradeep Gaur <ps652227@gmail.com>
2024-11-02 20:00:39 +0530

    recoil setup

--- 01_routing/src/main.jsx
+++ 01_routing/src/main.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { useState } from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter,Routes,Route,Outlet } from 'react-router-dom'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom } from './store/atoms/counter.js'
 
 const Router=
 <BrowserRouter>
@@ -15,9 +20,9 @@ const Router=
 </BrowserRouter>
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <RecoilRoot>
     {Router}
-  </StrictMode>,
+    </RecoilRoot>
 )
 
 function Layout(){
@@ -28,9 +33,73 @@ function Layout(){
     )
 }
 function About(){
-  return "About Us";
+
+
+  return (<>
+    <CurrentCount1 />
+    <IncreaseCount1 />
+    <DecreaseCount1 />
+    <p>Hello About</p>
+  </>);
+}
+
+
+function CurrentCount1(){
+  const counter=useRecoilValue(counterAtom);
+
+  return<>
+  <h6>{counter}</h6>
+  </>
+}
+
+function IncreaseCount1(){
+  const setCounter=useSetRecoilState(counterAtom);
+
+  function increaseCount(){
+    setCounter(c=>c+1);
+  }
+  return <button onClick={increaseCount}>Increase</button>
+}
+
+function DecreaseCount1(){
+  const setCounter=useSetRecoilState(counterAtom);
+
+  function decreaseCount(){
+    setCounter(c=>c-1);
+  }
+  return <button onClick={decreaseCount}>Decrease</button>
+
 }
 
 function Home(){
-  return "Home";
+  const [counter,setCounter]=useState(0);
+
+
+  return (<>
+    <CurrentCount counter={counter}/>
+    <IncreaseCount setCounter={setCounter}/>
+    <DecreaseCount setCounter={setCounter}/>
+  </>);
+}
+
+function CurrentCount({counter}){
+  return<>
+  <h6>{counter}</h6>
+  </>
+}
+
+function IncreaseCount({setCounter}){
+  function increaseCount(){
+    setCounter(c=>c+1);
+  }
+  return <button onClick={increaseCount}>Increase</button>
+}
+
+function DecreaseCount({setCounter}){
+
+  function decreaseCount(){
+    setCounter(c=>c-1);
+  }
+  return <button onClick={decreaseCount}>Decrease</button>
+
 }
---
+++ 02_recoil/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { useRecoilValue,useSetRecoilState } from 'recoil';
+import { counter } from './store/atoms/counter';
+import "./app.css";
+
+export default function App() {
+
+  return (
+    <>
+    <CurrentCount1 />
+    <IncreaseCount1 />
+    <DecreaseCount1 />
+    <p>Hello About</p>
+    </>
+  )
+}
+
+function CurrentCount1(){
+  const count=useRecoilValue(counter);
+
+  return<>
+  <h6>{count}</h6>
+  </>
+}
+
+function IncreaseCount1(){
+  const setCounter=useSetRecoilState(counter);
+
+  function increaseCount(){
+    setCounter(c=>c+1);
+  }
+  return <button onClick={increaseCount}>Increase</button>
+}
+
+function DecreaseCount1(){
+  const setCounter=useSetRecoilState(counter);
+
+  function decreaseCount(){
+    setCounter(c=>c-1);
+  }
+  return <button onClick={decreaseCount}>Decrease</button>
+
+}
+
